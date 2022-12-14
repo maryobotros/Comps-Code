c63a29ece0092f208ec55aea90e2530c0d7b3169
mmb1214 <78535223+mmb1214@users.noreply.github.com>
2022-12-14 03:44:32 -0800

    Added comments

--- Arduino_code/sketch_oct09b/sketch_oct09b.ino
+++ Arduino_code/sketch_oct09b/sketch_oct09b.ino
@@ -1,285 +1,352 @@
+// Servo Object
 # include <Servo.h>
 Servo myServo;
-int lightPin = 2;
-int const potPin = A0;
+// Servo angle 
 int angle;
-const int lightSensor = A1;
-int lightVal;
-int potVal;
-int x = 0;
 
+// Potetiometer pin
+int const potPin = A0;
+// Potentiometer value
+int potVal;
+
+// Phototransistor pin
+const int lightSensor = A1;
+// Phototransistor value
+int lightVal;
+
+// Temperature pin
 const int tempSensor = A5;
+// Temperature value
 int tempVal;
+// Variables for converting temp from Celsius to Farenheit
 float voltage;
 float tempInC;
 float tempInF;
 
-int threshold = 90;
-int tempThreshold = 90.0;
-int upperLimit = 180;
-int lowerLimit = 0;
+// Potentiometer threshold
+int threshold = 90; // set at 90 by default because it's in-between 0 and 180
+// Temperature threshold
+int tempThreshold = 90.0; // set at 90 degrees Farenheit by default
+// Upper limit
+int upperLimit = 180; // set at 180 by default because this is the max angle of the servo
+// Lower limit
+int lowerLimit = 0; // se at 0 by default bevause this is the min angle of the servo
+
+// LEDs for debugging
+int lightPin = 2;
+int yellowLight = 7;
+int greenLight = 3;
+
+// Variable for controlling modes
+int x = 0;
 
 
+// setup() code runs once
 void setup() {
-  pinMode(lightPin, OUTPUT);
+  // Initialize serial monitor
   Serial.begin(9600);
+  
+  // Servo motor is attatched to pin 9
   myServo.attach(9);
+
+  // All LEDs are outputs 
+  pinMode(lightPin, OUTPUT);
+  pinMode(yellowLight, OUTPUT);
+  pinMode(greenLight, OUTPUT);
 }
 
+
+// Main code in loop() runs repeatedly:
+// Code within this loop turns LEDs on and off for debugging and LEDs are not required for the program to run
 void loop() {
-  // Reading in real time
+  // Potentiometer Mode
   if(x == 0){
+    // Send info to the webpage that potentiometer mode is selected
+    Serial.println("Mode: Potentiometer");
     digitalWrite(lightPin, LOW);
 
+    // Read the value of the potetiometer
     potVal = analogRead(potPin);
-//    Serial.print("potVal: ");
-//    Serial.print(potVal);
 
+    // Scale the potentiometer's value and set the angle to it
     angle = map(potVal, 0, 1023, 0, 180);
-//    Serial.print(" , angle: ");
-//    Serial.println(angle);
 
+    // Set servo to the angle
     myServo.write(angle);
     delay(15);
 
+    // If angle of the servo is greater than the threshold
     if(angle >= threshold){
-      Serial.println("Open"); // Comment out in order to be able to be able to debug
+      // Send the webpage info that the blinds are open
+      Serial.println("Open");
+      digitalWrite(yellowLight, LOW);
+      digitalWrite(greenLight, HIGH);
     }
+    // Otherwise
     else if(angle < threshold){
-      Serial.println("Closed"); // Comment out in order to be able to be able to debug
+      // Send the webpage info that the blinds are closed
+      Serial.println("Closed"); 
+      digitalWrite(yellowLight, HIGH);
+      digitalWrite(greenLight, LOW);
     }
   }
+  
+  // Phototransistor Mode
   else if(x == 1){
+    // Send info to the webpage that phototransistor mode is selected
+    Serial.println("Mode: Phototransistor");
     digitalWrite(lightPin, HIGH);
-    
+
+    // Read the value from the phototransistor
     lightVal = analogRead(lightSensor);
+    
+    // If the light value is greater than 600 (default threshold for sensing light)
     if(lightVal > 600){
+       // Move the servo to the upper limit and 
+       // send the webpage info that the blinds are open
        myServo.write(upperLimit);
        Serial.println(upperLimit);
-       Serial.println("Open"); // Comment out in order to be able to be able to debug
+       Serial.println("Open"); 
+       digitalWrite(yellowLight, LOW);
+       digitalWrite(greenLight, HIGH);
      }
+     // Otherwise
      else{
+       // Move the servo to the lower limit and 
+       // send the webpage info that the blinds are closed
        myServo.write(lowerLimit);
        Serial.println(lowerLimit);
-       Serial.println("Closed"); // Comment out in order to be able to be able to debug
+       Serial.println("Closed"); 
+       digitalWrite(yellowLight, HIGH);
+       digitalWrite(greenLight, LOW);
      }
 
      delay(1000);
   }
+  
+  // Temperature Mode
   else if(x == 2){
-    digitalWrite(lightPin, LOW);
+    // Send info to the webpage that the temperature mode is selected
     Serial.println("Mode: Temperature");
+    digitalWrite(lightPin, LOW);
 
+    // Calculate the value of the temperature from the temp sensor
+    // and convert it to Farenheit
     tempVal = analogRead(tempSensor);
     voltage = (tempVal / 1024.0) * 5.0;
     tempInC = (voltage - .5) * 100;
     tempInF = (tempInC * 1.8) + 32;
     Serial.println(tempInF);
-  
+
+    // If the temperature is greater than the temperatur threshold
     if(tempInF >= tempThreshold){
+      // Move the servo to the lower limit and 
+      // send the webpage info that the blinds are closed
       myServo.write(lowerLimit);
       Serial.println(lowerLimit);
-      Serial.println("Closed"); // Comment out in order to be able to be able to debug
+      Serial.println("Closed"); 
+      digitalWrite(yellowLight, HIGH);
+      digitalWrite(greenLight, LOW);
     }
     else{
+      // Move the servo to the upper limit and 
+      // send the webpage info that the blinds are open
       myServo.write(upperLimit);
       Serial.println(upperLimit);
-      Serial.println("Open"); // Comment out in order to be able to be able to debug
+      Serial.println("Open"); 
+      digitalWrite(yellowLight, LOW);
+      digitalWrite(greenLight, HIGH);
     }
 
     delay(1000);  
   }
+
+  // Temperature or Phototransistor Mode
   else if(x == 3){
     Serial.println("Mode: Temperature or Light");
     
-    // Get the temperature
+    // Calculate the value of the temperature from the temp sensor
+    // and convert it to Farenheit
     tempVal = analogRead(tempSensor);
     voltage = (tempVal / 1024.0) * 5.0;
     tempInC = (voltage - .5) * 100;
     tempInF = (tempInC * 1.8) + 32;
     Serial.println(tempInF);
 
-    // Get the light
+    // Get the light value from the phototransistor
     lightVal = analogRead(lightSensor);
 
-    // Check temperature and light
+    // If the temperature is above the temperature threshold or the light is below the threshold
     if(tempInF >= tempThreshold || lightVal < 600){
+       // Move the servo to the lower limit and 
+       // send the webpage info that the blinds are closed
        myServo.write(lowerLimit);
        Serial.println(lowerLimit);
-       Serial.println("Closed"); // Comment out in order to be able to be able to debug
+       Serial.println("Closed"); 
+
+       digitalWrite(yellowLight, HIGH);
+       digitalWrite(greenLight, LOW);
      }
+     // Otherwise
      else{
+      // Move the servo to the upper limit and 
+      // send the webpage info that the blinds are open
       myServo.write(upperLimit);
       Serial.println(upperLimit);
-      Serial.println("Open"); // Comment out in order to be able to be able to debug
+      Serial.println("Open"); 
+
+      digitalWrite(yellowLight, LOW);
+      digitalWrite(greenLight, HIGH);
     }
 
     delay(1000);
   }
+
+  // Temperature and Phototransistor Mode
   else if(x == 4){
     Serial.println("Mode: Temperature and Light");
     
-    // Get the temperature
+    // Calculate the value of the temperature from the temp sensor
+    // and convert it to Farenheit
     tempVal = analogRead(tempSensor);
     voltage = (tempVal / 1024.0) * 5.0;
     tempInC = (voltage - .5) * 100;
     tempInF = (tempInC * 1.8) + 32;
     Serial.println(tempInF);
 
-    // Get the light
+    // Get the light value from the phototransistor
     lightVal = analogRead(lightSensor);
 
-    // Check temperature and light
-    if(tempInF >= tempThreshold && lightVal > 600){
+    // If the temperature is greater than or equal to the temperature threshold and there is no light
+    if(tempInF >= tempThreshold && lightVal < 600){
+       // Move the servo to the lower limit and 
+       // send the webpage info that the blinds are closed
        myServo.write(lowerLimit);
        Serial.println(lowerLimit);
-       Serial.println("Closed"); // Comment out in order to be able to be able to debug
+       Serial.println("Closed"); 
+
+       digitalWrite(yellowLight, HIGH);
+       digitalWrite(greenLight, LOW);
      }
      else{
+      // Move the servo to the upper limit and 
+      // send the webpage info that the blinds are open
       myServo.write(upperLimit);
       Serial.println(upperLimit);
-      Serial.println("Open"); // Comment out in order to be able to be able to debug
+      Serial.println("Open"); 
+
+      digitalWrite(yellowLight, LOW);
+      digitalWrite(greenLight, HIGH);
     }
 
     delay(1000);
   }
+
+  // Open blinds
   else if(x == 5){
     Serial.println("Mode: Open");
-    
+
+    // Move the servo to the upper limit and 
+    // send the webpage info that the blinds are open
     myServo.write(upperLimit);
-    Serial.println("Open"); // Comment out in order to be able to be able to debug
+    Serial.println("Open");
+
+    digitalWrite(yellowLight, LOW);
+    digitalWrite(greenLight, HIGH);
 
     delay(1000);
   }
+
+  // Close blinds 
   else if(x == 6){
     Serial.println("Mode: Close");
-    
+
+    // Move the servo to the lower limit and 
+    // send the webpage info that the blinds are closed
     myServo.write(lowerLimit);
     Serial.println("Closed"); // Comment out in order to be able to be able to debug
+
+    digitalWrite(yellowLight, HIGH);
+    digitalWrite(greenLight, LOW);
     
     delay(1000);
   }
   
-  
 
-  // Reading from nodejs
+  // Check to make sure Arduino is receiving information over serial port
   if(Serial.available() > 0){
     String receivedString = "";
 
+    // Loop that constantly reads information that is sent from the webapp through the serialport
+    // and puts it into receivedString variable 
     while(Serial.available() > 0){
       receivedString += char(Serial.read());
     }
 
-//    Serial.println(receivedString);
-
+    // User selects "Potentiometer Mode"
     if(receivedString == "0"){
+      // Set mode to "Potentiometer Mode"
       x = 0;
-      digitalWrite(lightPin, LOW);
-      Serial.println("Mode: Potentiometer");
-      
-      potVal = analogRead(potPin);
-//      Serial.print("potVal: ");
-//      Serial.print(potVal);
-
-      angle = map(potVal, 0, 1023, 0, 179);
-//      Serial.print(" , angle: ");
-//      Serial.println(angle);
-
-      myServo.write(angle);
-      delay(15);
     }
+
+    // User selects "Phototransistor Mode"
     else if (receivedString == "1"){
+      // Set mode to "Phototransistor Mode"
       x = 1;
-      digitalWrite(lightPin, HIGH);
-      Serial.println("Mode: Phototransisor");
-      
-      lightVal = analogRead(lightSensor);
-      if(lightVal > 600){
-         myServo.write(180);
-       }
-       else{
-        myServo.write(0);
-//        Serial.println("Closed");
-      }
     }
+
+    // User selects "Temperature Mode"
     else if(receivedString == "2"){
+      // Set mode to "Temperature Mode"
       x = 2;
-      digitalWrite(lightPin, LOW);
-      Serial.println("Mode: Temperature");
-
-      tempVal = analogRead(tempSensor);
-      voltage = (tempVal / 1024.0) * 5.0;
-      tempInC = (voltage - .5) * 100;
-      tempInF = (tempInC * 1.8) + 32;
-
-      
-      if(tempInF >= 90.0){
-         myServo.write(0);
-       }
-       else{
-        myServo.write(180);
-      }
-
-      delay(1000);
     }
+
+    // User selects "Temperature or Phototransistor" mode
     else if(receivedString == "3"){
+      // Set mode to "Temperature or Phototransistor"
       x = 3;
-      Serial.println("Mode: Temperature or Light");
-
-      // Get the temperature
-      tempVal = analogRead(tempSensor);
-      voltage = (tempVal / 1024.0) * 5.0;
-      tempInC = (voltage - .5) * 100;
-      tempInF = (tempInC * 1.8) + 32;
-
-      // Get the light
-      lightVal = analogRead(lightSensor);
-
-      // Check temperature and light
-      if(tempInF >= 90.0 || lightVal > 600){
-         myServo.write(0);
-       }
-       else{
-        myServo.write(180);
-      }
     }
+
+    // User selects "Temperature and Phototransistor" mode
     else if(receivedString == "4"){
+      // Set mode to "Temperature and Phototransistor"
       x = 4;
-      Serial.println("Mode: Temperature and Light");
-
-      // Get the temperature
-      tempVal = analogRead(tempSensor);
-      voltage = (tempVal / 1024.0) * 5.0;
-      tempInC = (voltage - .5) * 100;
-      tempInF = (tempInC * 1.8) + 32;
-
-      // Get the light
-      lightVal = analogRead(lightSensor);
-
-      // Check temperature and light
-      if(tempInF >= 90.0 && lightVal > 600){
-         myServo.write(0);
-       }
-       else{
-        myServo.write(180);
-      }
     }
+
+    // User clicks "Open" button
     else if(receivedString == "5"){
+      // Open blinds 
       x = 5;
     }
+    
+    // User clicks "Close" button 
     else if(receivedString == "6"){
+      // Close blinds
       x = 6;
     }
+
+    // User sets potentiometer threshold
     else if(receivedString.substring(0, 1) == "P"){
+      // Change potentiometer threshold to the value in the input box
       threshold = receivedString.substring(2, 4).toInt();
     }
+
+    // User sets temperature threshold
     else if(receivedString.substring(0, 1) == "T"){
+      // Change temperature threshold to value in the input box 
       tempThreshold = receivedString.substring(2, 4).toInt();
     }
+
+    // User sets upper limit 
     else if(receivedString.substring(0, 1) == "U"){
+       // Change upper limit to value in the input box
        upperLimit = receivedString.substring(2, 5).toInt();
     }
-    else if(receivedString.substring(0, 1) == "L")
+
+    // User sets lower limit 
+    else if(receivedString.substring(0, 1) == "L") {
+       // Change lower limit to value in the input box
        lowerLimit = receivedString.substring(2, 4).toInt();
     }
+  }
 }
--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,15 +1,21 @@
+// Start the server using http library and file system library to grab the index page
 var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync( 'index.html');
 
+// Load serialport library into SerialPort variable
 var SerialPort = require('serialport');
-const parsers = SerialPort.parsers;
 
+// Parser is created and tells Node.js to receive data coming in through serialport, reading each line
+const parsers = SerialPort.parsers;
 const parser = new parsers.Readline({
     delimiter: '\r\n'
 });
 
-var port = new SerialPort('/dev/tty.usbmodem14101',{ 
+// Port has to be opened up with the location of the port connected to the Arduino passed in as an argument.
+// This argument passed in on the line below '/dev..' will need to be substituted with your own port address
+// Connect Arduino to computer and use the command ls /dev/{tty,cu}.* in comand line to find your port address
+var port = new SerialPort('/dev/tty.usbmodem14101',{
     baudRate: 9600,
     dataBits: 8,
     parity: 'none',
@@ -17,45 +23,60 @@ var port = new SerialPort('/dev/tty.usbmodem14101',{
     flowControl: false
 });
 
+// Attatch port to parser object
 port.pipe(parser);
 
+// Server named 'app' is craeted and it will return the contents of the index.html file.
 var app = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(index);
 });
 
+// Socket has to be initialized, which will send the information from the Node.js server to the webpage.
 var io = require('socket.io').listen(app);
 
+// Connection event is executed
+// When a button is clicked on a webpage one of the functions inside this event
+// will send data to the Arduino to be read
 io.on('connection', function(socket) {
+    // Inidcate that Node is using port upon connection
     console.log('Node is listening to port');
     
+    // Function for sending selected mode 
     socket.on('modeSelector',function(data){
         console.log( data );
         port.write( data.status );
     });
 
-
+    // Function for sending potentiometer threshold
     socket.on('input-text', function(data){
         port.write('P ' + data);
         console.log('P ' + data);
     });
 
+    // Function for sending temperature threshold
     socket.on('input-temp', function(data){
         port.write('T ' + data);
         console.log('T ' + data);
     });
 
+    // Function for sending upper limit
     socket.on('input-upper', function(data){
         port.write('U ' + data);
         console.log('U ' + data);
     });
     
+    // Function for sending lower limit
     socket.on('input-lower', function(data){
         port.write('L ' + data);
         console.log('L ' + data);
     });
 });
 
+// Parser is given an on event whenever data is received
+// When the parser receives data, it will run the function below and this data will be console logged. 
+//Additionally, when data is received from the parser, which is data coming from the serialport, a message should be emitted. 
+// That message will be called ‘data’ and the data that will be passed will be the data variable.
 parser.on('data', function(data){
     console.log('Received data from port: ' + data);
 
